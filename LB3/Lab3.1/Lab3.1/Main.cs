32ed1a4b955522f41d68bca68792054ef2e7508c
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2022-10-03 14:14:25 +0300

    Lb3

---
+++ LB3/Lab3.1/Lab3.1/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double side1;
+            double side2;
+            Console.Write("1 сторона:");
+            side1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("2 сторона:");
+            side2 = Convert.ToDouble(Console.ReadLine());
+            Rectangle rectangle = new Rectangle(side1, side2);
+            rectangle.PerimeterCalculator();
+            rectangle.AreaCalculator();
+            Console.WriteLine($"Периметр - {rectangle.Perimeter}");
+            Console.WriteLine($"Площа - {rectangle.Area}");
+        }
+    }
+}
---
+++ LB3/Lab3_4/Lab3_4/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab3._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice = new Invoice(400, "Max", "TM");
+            Console.WriteLine(invoice.CalculateValueOrder());
+        }
+    }
+}
